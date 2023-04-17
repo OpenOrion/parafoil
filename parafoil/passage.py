463f6e96e78caaf0b2b0547cf51f12610a5dcbfd
Afshawn Lotfi <me@afshawn.org>
2023-04-17 03:31:17 +0000

    beggining to add simulations

--- parafoil/passage.py
+++ parafoil/passage.py
@@ -1,21 +1,30 @@
-from dataclasses import dataclass
+from dataclasses import asdict, dataclass, field
 from functools import cached_property
-from typing import List, Literal, Optional, Union
+from typing import Any, Dict, List, Literal, Optional, Union
 import numpy as np
 import numpy.typing as npt
 from plotly import graph_objects as go
 from parafoil.airfoil import Airfoil
 from parafoil.bspline import get_bspline
-from ezmesh import Geometry, CurveLoop, PlaneSurface, BoundaryLayer
-
-@dataclass
-class PassageOutline:
-    top_coords: npt.NDArray[np.float64]
-    bottom_coords: npt.NDArray[np.float64]
+from ezmesh import Geometry, CurveLoop, PlaneSurface, BoundaryLayerField
+from paraflow import Passage, FlowState
 
 
 @dataclass
-class Passage:
+class MeshParameters:
+    top_label: str = "top"
+    bottom_label: str = "bottom"
+    inlet_label: str = "inlet"
+    outlet_label: str = "outlet"
+    airfoil_label: Union[str, List[str]] = "airfoil"
+    airfoil_mesh_size: Optional[float] = None
+    boundary_layer_thickness: Optional[float] = None
+    boundary_wall_mesh_size: Optional[float] = None
+    passage_mesh_size: Optional[float] = None
+
+
+@dataclass
+class AirfoilPassage(Passage):
     airfoil: Airfoil
     "airfoil for the passage"
 
@@ -34,8 +43,14 @@ class Passage:
     outline_type: Literal['camber', 'line'] = 'camber'
     "type of outline for the passage"
 
-    offset: npt.NDArray[np.float64] = np.array([0, 0])
+    offset: List[float] = field(default_factory=lambda: [0, 0])
     "offset of the passage"
+
+    mesh_params: MeshParameters = field(default_factory=MeshParameters)
+    "mesh parameters for the passage"
+
+    def __post_init__(self):
+        self.inlet_length = self.outlet_length = self.total_spacing
 
     @cached_property
     def total_spacing(self):
@@ -43,43 +58,20 @@ class Passage:
 
     @cached_property
     def coords(self) -> npt.NDArray[np.float64]:
-        return np.concatenate((self.outline.top_coords, self.outline.bottom_coords))
+        passage_bspline = get_bspline(self.camber_ctrl_pnts, 3)
+        passage_bspline_coords = passage_bspline(self.airfoil.sampling)
+        top_coords = passage_bspline_coords + np.array([0, self.total_spacing/2]) + self.offset
+        bottom_coords = np.flip(passage_bspline_coords, axis=0) + np.array([0, -self.total_spacing/2]) + self.offset
+        return np.concatenate((top_coords, bottom_coords))
 
     @cached_property
-    def outline(self):
+    def camber_ctrl_pnts(self):
         camber_coords = self.airfoil.camber_coords + np.array([self.leading_edge_gap, 0])
-        outline_ctrl_pnts = np.concatenate([
+        return np.concatenate([
             np.array([[0, 0], [self.leading_edge_gap, 0]]),
             camber_coords,
             np.array([camber_coords[-1] + np.array([self.trailing_edge_gap, 0])])    # type: ignore
         ])
-        passage_bspline = get_bspline(outline_ctrl_pnts, 3)
-        passage_bspline_coords = passage_bspline(self.airfoil.sampling)
-        if self.outline_type == 'camber':
-            outline_coords = passage_bspline_coords
-        else:
-            start_coord = passage_bspline_coords[0]
-            line_slope_start_coord: Optional[npt.NDArray[np.float64]] = np.array([self.leading_edge_gap, 0])
-            
-            is_increasing = passage_bspline_coords[1][1] > start_coord[1]
-            for coord in passage_bspline_coords[1:]:
-                if is_increasing and coord[1] <= start_coord[1]:
-                    line_slope_start_coord = coord
-                    break
-                elif not is_increasing and coord[1] >= start_coord[1]:
-                    line_slope_start_coord = coord
-                    break
-            
-            outline_coords: npt.NDArray[np.float64] = np.concatenate([
-                np.array([[0, line_slope_start_coord[1]], line_slope_start_coord]),
-                np.array([camber_coords[-1]]),
-                np.array([camber_coords[-1] + np.array([self.trailing_edge_gap, 0])])    # type: ignore
-            ])
-
-        return PassageOutline(
-            top_coords=outline_coords + np.array([0, self.total_spacing/2]) + self.offset,
-            bottom_coords=np.flip(outline_coords, axis=0) + np.array([0, -self.total_spacing/2]) + self.offset
-        )
 
     @cached_property
     def airfoils_coords(self):
@@ -92,76 +84,128 @@ class Passage:
             (self.total_spacing/2) - (self.spacing/2) - airfoil_leading_pnt[1]
         ])
         for i in range(self.num_blades):
-            airfoil_offseted_coords = airfoil_coords+airfoil_offset-np.array([0, i*self.spacing]) + self.offset
+            airfoil_offseted_coords = airfoil_coords+airfoil_offset-np.array([0, i*self.spacing]) + self.offset  # type: ignore
             airfoils_coords.append(airfoil_offseted_coords)
         return airfoils_coords
 
-    def generate_mesh(
+    def get_mesh(
         self,
-        top_label: str = "top",
-        bottom_label: str = "bottom",
-        inlet_label: str = "inlet",
-        outlet_label: str = "outlet",
-        airfoil_label: Union[str, List[str]] = "airfoil",
-        surface_label: Optional[str] = None,
-        airfoil_mesh_size: Optional[float] = None,
-        boundary_layer_thickness: Optional[float] = None,
-        boundary_wall_mesh_size: Optional[float] = None,
-        passage_mesh_size: Optional[float] = None,
         output_path: Optional[str] = None
     ):
-        if airfoil_mesh_size is None: 
-            airfoil_mesh_size = 0.02 * self.airfoil.chord_length
-        if boundary_layer_thickness is None: 
-            boundary_layer_thickness = 0.01 * self.airfoil.chord_length
-        if boundary_wall_mesh_size is None: 
-            boundary_wall_mesh_size = 0.001 * self.airfoil.chord_length
-        if passage_mesh_size is None: 
-            passage_mesh_size = 0.05 * self.airfoil.chord_length
-
+        if self.mesh_params.airfoil_mesh_size is None:
+            self.mesh_params.airfoil_mesh_size = 0.02 * self.airfoil.chord_length
+        if self.mesh_params.boundary_layer_thickness is None:
+            self.mesh_params.boundary_layer_thickness = 0.01 * self.airfoil.chord_length
+        if self.mesh_params.boundary_wall_mesh_size is None:
+            self.mesh_params.boundary_wall_mesh_size = 0.001 * self.airfoil.chord_length
+        if self.mesh_params.passage_mesh_size is None:
+            self.mesh_params.passage_mesh_size = 0.05 * self.airfoil.chord_length
 
         with Geometry() as geometry:
+            ctrl_pnts = self.camber_ctrl_pnts
             passage_curve_loop = CurveLoop.from_coords(
-                self.coords, 
-                mesh_size = passage_mesh_size,
-                groups=[self.outline.top_coords, self.outline.bottom_coords],
-                labels=[top_label, outlet_label, bottom_label, inlet_label],
+                [
+                    ("BSpline", ctrl_pnts + np.array([0, self.total_spacing/2]) + self.offset),
+                    ("BSpline", ctrl_pnts[::-1] + np.array([0, -self.total_spacing/2]) + self.offset)
+                ],
+                mesh_size=self.mesh_params.passage_mesh_size,
+                labels=[self.mesh_params.top_label, self.mesh_params.outlet_label, self.mesh_params.bottom_label, self.mesh_params.inlet_label],
             )
 
-            boundary_layer = BoundaryLayer(
-                hwall_n=boundary_wall_mesh_size,
-                thickness=boundary_layer_thickness,
-                is_quad_mesh=True,
-                intersect_metrics=False
-            )
-
-            if isinstance(airfoil_label, List):
-                assert len(airfoil_label) == len(self.airfoils_coords)
-
-
+            if isinstance(self.mesh_params.airfoil_label, List):
+                assert len(self.mesh_params.airfoil_label) == len(self.airfoils_coords)
 
             airfoil_curve_loops = [
                 CurveLoop.from_coords(
-                    airfoil_coords, 
-                    mesh_size = airfoil_mesh_size, 
-                    labels=airfoil_label[i] if isinstance(airfoil_label, List) else airfoil_label,
-                    fields=[boundary_layer],
+                    airfoil_coords,
+                    mesh_size=self.mesh_params.airfoil_mesh_size,
+                    labels=self.mesh_params.airfoil_label[i] if isinstance(self.mesh_params.airfoil_label, List) else self.mesh_params.airfoil_label,
+                    fields=[
+                        BoundaryLayerField(
+                            hwall_n=self.mesh_params.boundary_wall_mesh_size,
+                            thickness=self.mesh_params.boundary_layer_thickness,
+                            is_quad_mesh=True,
+                            intersect_metrics=False
+                        )
+                    ],
                 )
                 for i, airfoil_coords in enumerate(self.airfoils_coords)
             ]
 
             surface = PlaneSurface(
                 outlines=[passage_curve_loop],
-                holes=airfoil_curve_loops,
-                label=surface_label
+                holes=airfoil_curve_loops
             )
             if output_path is not None:
                 geometry.write(output_path)
             return geometry.generate(surface)
 
-    def visualize(self):
+    def get_config(self, inflow: FlowState, working_directory: str, id: str):
+        return {
+            "SOLVER": "RANS",
+            "KIND_TURB_MODEL": "SST",
+            "MATH_PROBLEM": "DIRECT",
+            "RESTART_SOL": "NO",
+            "SYSTEM_MEASUREMENTS": "SI",
+            "MACH_NUMBER": inflow.mach_number,
+            "AOA": 0.0,
+            "SIDESLIP_ANGLE": 0.0,
+            "INIT_OPTION": "TD_CONDITIONS",
+            "FREESTREAM_OPTION": "TEMPERATURE_FS",
+            "FREESTREAM_PRESSURE": inflow.total_state.P,
+            "FREESTREAM_TEMPERATURE": inflow.total_state.T,
+            "FREESTREAM_DENSITY": inflow.total_state.rho_mass(),
+            "REF_DIMENSIONALIZATION": "DIMENSIONAL",
+            "FLUID_MODEL": "IDEAL_GAS",
+            "GAMMA_VALUE": inflow.gamma,
+            "GAS_CONSTANT": inflow.gas_constant,
+            "VISCOSITY_MODEL": "SUTHERLAND",
+            "MU_REF": 1.716E-5,
+            "MU_T_REF": 273.15,
+            "SUTHERLAND_CONSTANT": 110.4,
+            "CONDUCTIVITY_MODEL": "CONSTANT_PRANDTL",
+            "AVERAGE_PROCESS_KIND": "MIXEDOUT",
+            "PERFORMANCE_AVERAGE_PROCESS_KIND": "MIXEDOUT",
+            "MIXEDOUT_COEFF": "(1.0, 1.0E-05, 15)",
+            "AVERAGE_MACH_LIMIT": 0.05,
+            "NUM_METHOD_GRAD": "WEIGHTED_LEAST_SQUARES",
+            "CFL_NUMBER": 10.0,
+            "CFL_ADAPT": "NO",
+            "CFL_ADAPT_PARAM": "( 1.3, 1.2, 1.0, 10.0)",
+            "LINEAR_SOLVER": "FGMRES",
+            "LINEAR_SOLVER_PREC": "LU_SGS",
+            "LINEAR_SOLVER_ERROR": 1E-6,
+            "LINEAR_SOLVER_ITER": 10,
+            "VENKAT_LIMITER_COEFF": 0.05,
+            "LIMITER_ITER": 999999,
+            "CONV_NUM_METHOD_FLOW": "ROE",
+            "MUSCL_FLOW": "YES",
+            "SLOPE_LIMITER_FLOW": "VAN_ALBADA_EDGE",
+            "ENTROPY_FIX_COEFF": 0.1,
+            "JST_SENSOR_COEFF": (0.5, 0.02),
+            "TIME_DISCRE_FLOW": "EULER_IMPLICIT",
+            "CONV_NUM_METHOD_TURB": "SCALAR_UPWIND",
+            "TIME_DISCRE_TURB": "EULER_IMPLICIT",
+            "CFL_REDUCTION_TURB": 1.0,
+            "OUTER_ITER": 21,
+            "CONV_RESIDUAL_MINVAL": -16,
+            "CONV_STARTITER": 10,
+            "CONV_CAUCHY_ELEMS": 100,
+            "CONV_CAUCHY_EPS": 1E-6,
+            "MESH_FILENAME": f"{working_directory}/passage{id}.su2",
+            "MESH_FORMAT": "SU2",
+            "TABULAR_FORMAT": "CSV",
+            "VOLUME_FILENAME": f"{working_directory}/flow{id}",
+            "RESTART_FILENAME":  f"{working_directory}/restart_flow{id}.dat",
+            "SURFACE_FILENAME":  f"{working_directory}/surface_flow{id}",
+            "CONV_FILENAME": f"{working_directory}/history{id}",
+            "OUTPUT_WRT_FREQ": 1000,
+            "SCREEN_OUTPUT": "(INNER_ITER, RMS_DENSITY, RMS_TKE, RMS_DISSIPATION, LIFT, DRAG)",
+        }
+
+    def visualize(self, title: str = "Passage"):
         fig = go.Figure(
-            layout=go.Layout(title=go.layout.Title(text="Passage"))
+            layout=go.Layout(title=go.layout.Title(text=title))
         )
         fig.add_trace(go.Scatter(
             x=self.coords[:, 0],
@@ -183,3 +227,58 @@ class Passage:
             ))
         fig.layout.yaxis.scaleanchor = "x"  # type: ignore
         fig.show()
+
+    def to_dict(self) -> Dict[str, Any]:
+        return asdict(self)
+
+
+@dataclass
+class AxialTurboPassage(Passage):
+    inflow_passage: AirfoilPassage
+    "Passage with inflow boundary condition"
+
+    outflow_passage: AirfoilPassage
+    "Passage with outflow boundary condition"
+
+    def __post_init__(self):
+        self.inlet_length = self.inflow_passage.inlet_length
+        self.outlet_length = self.outflow_passage.outlet_length
+
+    def get_config(self, inflow: FlowState, target_outflow: FlowState, working_directory: str, id: str):
+        inflow_config = self.inflow_passage.get_config(inflow, working_directory, id)
+        inflow_mesh_params = self.inflow_passage.mesh_params
+        outflow_mesh_params = self.outflow_passage.mesh_params
+        return {
+            **inflow_config,
+            "MULTIZONE": "YES",
+            "CONFIG_LIST": {
+                "zone_1.cfg": {
+                    **({
+                        "GRID_MOVEMENT": "STEADY_TRANSLATION",
+                        "MACH_MOTION": 0.35,
+                        "TRANSLATION_RATE": f"0.0 {inflow.translation_velocity} 0.0",
+                    } if inflow.translation_velocity else {"GRID_MOVEMENT": "NONE"})
+                },
+                "zone_2.cfg": {
+                    **({
+                        "GRID_MOVEMENT": "STEADY_TRANSLATION",
+                        "MACH_MOTION": 0.35,
+                        "TRANSLATION_RATE": f"0.0 {target_outflow.translation_velocity} 0.0",
+                    } if target_outflow.translation_velocity else {"GRID_MOVEMENT": "NONE"})
+                }
+            },
+            "MARKER_HEATFLUX": f"( {inflow_mesh_params.airfoil_label}, 0.0, {outflow_mesh_params.airfoil_label}, 0.0)",
+            "MARKER_PERIODIC": f"( {inflow_mesh_params.bottom_label}, {inflow_mesh_params.top_label}, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, {self.inflow_passage.spacing}, 0.0, {outflow_mesh_params.bottom_label}, {outflow_mesh_params.top_label}, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, {self.outflow_passage.spacing}, 0.0)",
+            "MARKER_TURBOMACHINERY": f"({inflow_mesh_params.inlet_label}, {inflow_mesh_params.outlet_label}, {outflow_mesh_params.inlet_label}, {outflow_mesh_params.outlet_label})",
+            "MARKER_MIXINGPLANE_INTERFACE": f"({inflow_mesh_params.outlet_label}, {inflow_mesh_params.inlet_label})",
+            "MARKER_GILES": f"({inflow_mesh_params.inlet_label}, TOTAL_CONDITIONS_PT, {inflow.total_state.P}, {inflow.total_state.T}, 1.0, 0.0, 0.0,1.0,1.0, {inflow_mesh_params.outlet_label}, MIXING_OUT, 0.0, 0.0, 0.0, 0.0, 0.0,1.0,1.0, {inflow_mesh_params.inlet_label}, MIXING_IN, 0.0, 0.0, 0.0, 0.0, 0.0,1.0, 1.0 {inflow_mesh_params.outlet_label}, STATIC_PRESSURE, {target_outflow.static_state.P}, 0.0, 0.0, 0.0, 0.0,1.0,1.0)",
+            "SPATIAL_FOURIER": "NO",
+            "TURBOMACHINERY_KIND": "AXIAL AXIAL",
+            "TURBULENT_MIXINGPLANE": "YES",
+            "RAMP_OUTLET_PRESSURE": "NO",
+            "RAMP_OUTLET_PRESSURE_COEFF": "(140000.0, 10.0, 2000)",
+            "MARKER_PLOTTING": f"({inflow_mesh_params.airfoil_label}, {outflow_mesh_params.airfoil_label})",
+        }
+
+    def to_dict(self) -> Dict[str, Any]:
+        return asdict(self)
