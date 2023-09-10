5d51ec897ced10da5e2c358b5e149e032f15c7dc
Afshawn Lotfi <me@afshawn.org>
2023-09-10 15:49:53 -0700

    added windows support

--- parafoil/passages/circular.py
+++ parafoil/passages/circular.py
@@ -3,7 +3,7 @@ from functools import cached_property
 from typing import Any, Dict, Optional
 import numpy as np
 from plotly import graph_objects as go
-from paraflow import Passage, SimulationOptions
+from paraflow import Passage, SimulationParams
 from parafoil.airfoils import Airfoil
 from ezmesh import Geometry, CurveLoop, PlaneSurface
 from parafoil.utils import get_sampling
@@ -40,7 +40,7 @@ class CircularPassage(Passage):
         y = self.radius * np.sin(theta)
         return np.column_stack((x, y))
 
-    def get_surfaces(self, sim_options: Optional[SimulationOptions] = None):
+    def get_surfaces(self, params: Optional[SimulationParams] = None):
         if self.mesh_params.airfoil_mesh_size is None:
             self.mesh_params.airfoil_mesh_size = 0.1 * self.airfoil.chord_length
         if self.mesh_params.passage_mesh_size is None:
@@ -93,7 +93,7 @@ class CircularPassage(Passage):
 
     def get_config(
         self,
-        sim_options: SimulationOptions,
+        sim_options: SimulationParams,
         working_directory: str,
         id: str,
     ) -> Dict[str, Any]:
