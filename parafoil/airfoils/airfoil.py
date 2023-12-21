71b0f3bf1e8dfcbf5be875f5a2b0554a1b50db31
Afshawn Lotfi <6283745+afshawnlotfi@users.noreply.github.com>
2023-12-21 00:00:06 -0500

    Added NACA airfoil support and added ability to add in angles as degrees (#26)

--- parafoil/airfoils/airfoil.py
+++ parafoil/airfoils/airfoil.py
@@ -1,4 +1,5 @@
-from typing import Protocol, Tuple
+from typing import Protocol
+import plotly.graph_objects as go
 import numpy.typing as npt
 import numpy as np
 
@@ -7,3 +8,20 @@ class Airfoil(Protocol):
     axial_chord_length: float
     height: float
     def get_coords(self) -> npt.NDArray[np.float64]: pass # type: ignore
+
+
+    def visualize(self):
+        fig = go.Figure(
+            layout=go.Layout(title=go.layout.Title(text="Airfoil"))
+        )
+
+        coords = self.get_coords()
+        fig.add_trace(go.Scatter(
+            x=coords[:, 0],
+            y=coords[:, 1],
+            name=f"Airfoil"
+        ))
+
+        fig.layout.yaxis.scaleanchor = "x"  # type: ignore
+
+        fig.show()
